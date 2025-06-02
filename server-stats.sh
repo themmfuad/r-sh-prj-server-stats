88025e137e2478c8d2baf0113ad79465d4a5a569
themmfuad <123576984+themmfuad@users.noreply.github.com>
2025-06-02 18:31:51 +0600

    Added 'Total top 5 cpu usage process to the script'

--- server-stats.sh
+++ server-stats.sh
@@ -3,11 +3,14 @@
 #STEP-1: install sysstat if not to run mpstat
 (apt-get update && apt-get install -y sysstat) > /dev/null 2>&1
 
-echo "1. Total CPU usage:"
+echo "[1.] Total CPU usage:\n.........................................................."
 mpstat | grep 'all' | awk '{print 100-$NF "%\n"}'
 
-echo "2. Total memory usage (Free vs Used including percentage):"
+echo "[2.] Total memory usage (Free vs Used including percentage):\n.........................................................."
 free | grep 'Mem:' | awk '{print "total used: " $3/1024 "MiB (" ($3/$2)*100 "%)" "\ntotal free: " $4/1024 "MiB (" ($4/$2)*100 "%)"}'
 
-echo "\n3. Total disk usage (Free vs Used including percentage):"
+echo "\n[3.] Total disk usage (Free vs Used including percentage):\n.........................................................."
 df -h / | grep "dev" | awk '{print "used: " $3 "(" $5 ")\n" "free: " $4 "(" 100-$5 ")"}'
+
+echo "\n[4.] Top 5 processes by CPU usage:\n.........................................................."
+ps -eo pid,ppid,cmd,%cpu --sort=-%cpu | grep -v "ps -eo" | head -n 6
