71ba265de7723c84bc96030bcc4f4f75e830a1d9
themmfuad <123576984+themmfuad@users.noreply.github.com>
2025-06-02 18:13:24 +0600

    Added 'Total disk usage to the script'

--- server-stats.sh
+++ server-stats.sh
@@ -8,5 +8,6 @@ mpstat | grep 'all' | awk '{print 100-$NF "%\n"}'
 
 echo "2. Total memory usage (Free vs Used including percentage):"
 free | grep 'Mem:' | awk '{print "total used: " $3/1024 "MiB (" ($3/$2)*100 "%)" "\ntotal free: " $4/1024 "MiB (" ($4/$2)*100 "%)"}'
-echo "Total disk usage (Free vs Used including percentage):\n"
 
+echo "\n3. Total disk usage (Free vs Used including percentage):"
+df -h / | grep "dev" | awk '{print "used: " $3 "(" $5 ")\n" "free: " $4 "(" 100-$5 ")"}'
